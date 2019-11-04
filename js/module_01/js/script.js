9936d781c9a8fb756b70c8ff17ad199373746ccd
Migho <mi.sidorenko@gmail.com>
2019-11-04 23:02:05 +0200

    added_trying

--- js/module_01/js/script.js
+++ js/module_01/js/script.js
@@ -1,7 +1,13 @@
 'use strict';
 
 
-let firstVar;
-firstVar = 3 * 7;
-console.log(firstVar)
-console.log(typeof firstVar);
+const name = 'protectiveFieldGenerator';
+let price = 1000;
+const string = `${name} selected, unit price ${price} credits`;
+
+console.log(string);
+
+price = 2000;
+const string2 = `${name} selected, unit price ${price} credits`;
+
+console.log(string2);
