cb0a9a792d18775c34b6426b5fe1f2db2d777bc5
Migho <mi.sidorenko@gmail.com>
2019-11-07 23:06:19 +0200

    t_2 a

--- js/module_01/js/script.js
+++ js/module_01/js/script.js
@@ -1,11 +1,24 @@
-'use strict';
+"use strict";
 
 //Task 1
-const name = 'protectiveFieldGenerator';
+const name = "protectiveFieldGenerator";
 let price = 1000;
 let string = `${name} selected, unit price ${price} credits`;
 
 console.log(string);
 
 price = 2000;
-console.log(string);
+let string1 = `${name} selected, unit price ${price} credits`;
+console.log(string1);
+
+//Task 2
+let type;
+const total = 100;
+const ordered = 20;
+type =
+  total >= ordered
+    ? "The order is placed, the manager will contact you!"
+    : "Not enough goods in stock!";
+console.log(type);
+
+//Task 3
