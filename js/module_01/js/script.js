1904da045449db4da069aa4601b2089709f3a0bc
Migho <mi.sidorenko@gmail.com>
2019-11-13 00:06:45 +0200

    t_3_22

--- js/module_01/js/script.js
+++ js/module_01/js/script.js
@@ -16,19 +16,30 @@ let type;
 const total = 100;
 const ordered = 20;
 type =
-  total >= ordered
-    ? "The order is placed, the manager will contact you!"
-    : "Not enough goods in stock!";
+  total >= ordered ?
+  "The order is placed, the manager will contact you!" :
+  "Not enough goods in stock!";
 console.log(type);
 
 //Task 3
 const ADMIN_PASSWORD = 'jqueryismyjam';
 let message;
-while (true) {
-  ADMIN_PASSWORD = prompt("Enter password")
-  
-  if ( ADMIN_PASSWORD === null) {
-    massage = 'canceled by user';
-    break;
-  } 
+
+message = prompt("Enter password", ""); { 
+
+  if (message < prompt) {
+    message == "Canceled by user!";
+  };
+
+  if (message === ADMIN_PASSWORD) {
+
+    message == 'Welcome';
+
+  };
+
+  if (message != ADMIN_PASSWORD) {
+    message == 'Access denied, wrong password!';
+  }
 }
+
+alert(message);
